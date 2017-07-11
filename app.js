e20844b3236dff87cf996700af71f6b96d5b3919
Valerijpl <valerijpll@gmail.com>
2017-07-11 17:52:19 +0300

    Remove unnesessary configuration in app.js

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
 var app = angular.module("myApp", ['ngRoute']);
-
-
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller: "HomeController",
-      url: '/list',
-      templateUrl: "list.html"
-    })
-    .when('/products/:id', {
-      url: '/products/ :id',
-      controller: 'ProductController',
-      templateUrl: 'product.html'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
-});
